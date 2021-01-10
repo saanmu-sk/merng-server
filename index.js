a51bfc67234afd96105eb99b1365a25cb0253230
SaanmuSk <saanmusk@gmail.com>
2021-01-10 21:14:32 +0530

    change port name

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const { MONGODB } = require('./config')
 const typeDefs = require('./graphql/typeDefs')
 const resolvers = require('./graphql/resolvers')
 
-const PORT = process.env.port || 5000
+const PORT = process.env.PORT || 5000
 
 const pubSub = new PubSub()
 const server = new ApolloServer({
